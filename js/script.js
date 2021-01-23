7064501dd2612e035541ac843730ea198dcbcba5
Avalanche322 <avalfnche@gmail.com>
2021-01-23 20:20:25 +0200

    highlight active anchor link

--- js/script.js
+++ js/script.js
@@ -47,3 +47,32 @@ $(document).ready(function(){
     $('body').toggleClass('lock');
   });
 });
+
+jQuery(window).scroll(function(){
+         var $sections = $('div');
+  $sections.each(function(i,el){
+        var top  = $(el).offset().top-20;
+        var bottom = top +$(el).height();
+        var scroll = $(window).scrollTop();
+        var id = $(el).attr('id');
+      if( scroll > top && scroll < bottom){
+            $('a.active').removeClass('active');
+      $('a[href="#'+id+'"]').addClass('active');
+
+        }
+    })
+ });
+
+$("nav").on("click","a", function (event) {
+    // исключаем стандартную реакцию браузера
+    event.preventDefault();
+
+    // получем идентификатор блока из атрибута href
+    var id  = $(this).attr('href'),
+
+    // находим высоту, на которой расположен блок
+        top = $(id).offset().top;
+     
+    // анимируем переход к блоку, время: 800 мс
+    $('body,html').animate({scrollTop: top}, 800);
+});

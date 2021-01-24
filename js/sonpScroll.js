04bf1d651b87ea33a5ee5b0a28db359823e9e44b
Avalanche322 <avalfnche@gmail.com>
2021-01-24 11:14:14 +0200

    fix 3 adapted for tablet

--- js/sonpScroll.js
+++ js/sonpScroll.js
@@ -11,7 +11,7 @@
 
  ******************************************************************/
 function windowSize(){
-    if ($(window).width() >= '1050'){
+    if ($(window).width() >= '1100'){
         var pagesMax = 0;
 
         $(document).ready(function(){

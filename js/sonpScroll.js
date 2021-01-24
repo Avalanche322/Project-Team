97c65a28f37e593d9bd23b80bc9ead43f16d65c3
Avalanche322 <avalfnche@gmail.com>
2021-01-24 11:40:25 +0200

    test

--- js/sonpScroll.js
+++ js/sonpScroll.js
@@ -11,7 +11,7 @@
 
  ******************************************************************/
 function windowSize(){
-    if ($(window).width() >= '1300'){
+    if ($(window).width() >= '1400'){
         var pagesMax = 0;
 
         $(document).ready(function(){
